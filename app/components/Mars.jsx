de4d292afd607a463021a7bcf3a6ba2dd5a21df3
THACHER <thachert1d@gmail.com>
2016-12-01 10:27:19 -0800

    modal working

--- app/components/Mars.jsx
+++ app/components/Mars.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 const Mars = React.createClass({
+  getInitialState() {
+    return {
+      open: false
+    }
+  },
+  handleOpen() {
+    this.setState({open: true});
+  },
+  handleClose() {
+    this.setState({open: false});
+  },
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
     return (
       <main>
         <h1>Mars</h1>
+        <FlatButton label="Modal Dialog" onClick={this.handleOpen} />
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          Only actions can close this dialog.
+        </Dialog>
         <div>
           <FlatButton label="earth" default={true} href="/"/>
           <FlatButton label="moon" primary={true} href="/moon"/>
--- app/components/Moon.jsx
+++ app/components/Moon.jsx
@@ -1,7 +1,28 @@
+import axios from 'axios';
+
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 
+
 const Moon = React.createClass({
+  // getInitialState() {
+  //   return {
+  //     movies: [],
+  //     jobs: [],
+  //     loadErr: false,
+  //    }
+  // },
+
+  // componentDidMount() {
+  //   axios.get(`http://www.omdbapi.com/?s=bob`)
+  //     .then(res => {
+  //       this.setState({ movies: res.Search });
+  //     })
+  //     .catch(err => {
+  //       this.setState({ loadErr: err });
+  //     });
+  // },
+
   render() {
     return (
       <main>
@@ -9,6 +30,22 @@ const Moon = React.createClass({
       <FlatButton label="earth" default={true} href="/"/>
       <FlatButton label="moon" primary={true} href="/moon"/>
       <FlatButton label="mars" secondary={true} href="/mars"/>
+
+        {/* <div>
+          <h1>Jobs!</h1>
+          {this.state.jobs.map(function(job) {
+            return (
+              <div key={job.id} className="job">
+                <a href={job.url}>
+                  {job.company_name}
+                  is looking for a
+                  {job.term}
+                  {job.title}
+                </a>
+              </div>
+            );
+          })}
+        </div> */}
       </main>
     )
   }
