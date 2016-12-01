5b864102a5e7684ba9d50b1a87cc235dc1a8e7b3
THACHER <thachert1d@gmail.com>
2016-12-01 13:37:28 -0800

    axios working css started

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,12 +1,18 @@
 import { link } from 'react-router'
 import NavBar from './NavBar'
 import React from 'react'
+const style = {
+  width: 1200,
+  margin: 0
+};
 
 const Header = React.createClass({
+
   render() {
     return (
       <header>
         <NavBar />
+        <img style={style} src={'http://edge.alluremedia.com.au/m/l/2015/09/Mars-Discovery.jpg'} />
       </header>
     )
   }
--- app/components/Moon.jsx
+++ app/components/Moon.jsx
@@ -7,49 +7,35 @@ import FlatButton from 'material-ui/FlatButton';
 const Moon = React.createClass({
   getInitialState() {
     return {
-      jobs: [],
+      movies: [],
       loadErr: false,
      }
   },
 
-  componentDidMount: function() {
-    var _this = this;
-    this.serverRequest =
-      axios
-        .get("http://codepen.io/jobs.json")
-        .then(function(result) {
-          _this.setState({
-            jobs: result.data.jobs
-          });
-        })
-  },
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
+  componentDidMount() {
+    axios.get(`http://www.omdbapi.com/?s=bob`)
+      .then(res => {
+        this.setState({ movies: res.data.Search });
+      })
+      .catch(err => {
+        this.setState({ loadErr: err });
+      });
   },
 
   render() {
     return (
       <main>
-      <h1>Moon</h1>
-      <FlatButton label="earth" default={true} href="/"/>
-      <FlatButton label="moon" primary={true} href="/moon"/>
-      <FlatButton label="mars" secondary={true} href="/mars"/>
+        <h1>Moon</h1>
+        <FlatButton label="earth" default={true} href="/"/>
+        <FlatButton label="moon" primary={true} href="/moon"/>
+        <FlatButton label="mars" secondary={true} href="/mars"/>
 
         <div>
-          <h1>Jobs!</h1>
-          {this.state.jobs.map(function(job) {
-            return (
-              <div key={job.id} className="job">
-                <a href={job.url}>
-                  {job.company_name}
-                  is looking for a
-                  {job.term}
-                  {job.title}
-                </a>
-              </div>
-            );
-          })}
+          <ul>
+          {this.state.movies.map((movie, index) =>
+            <li key={index}>{movie.Title} | {movie.Year}</li>
+          )}
+          </ul>
         </div>
       </main>
     )
