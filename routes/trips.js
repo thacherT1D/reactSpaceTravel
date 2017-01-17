0126cacfbf2a3e825183b0683be6b88bf03a3a84
THACHER <thachert1d@gmail.com>
2017-01-16 16:36:08 -0800

    routing for users, trips and destinations - no auth

--- routes/trips.js
+++ routes/trips.js
@@ -1,6 +1,23 @@
 'use strict';
 
+const boom = require('boom');
 const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
 const router = express.Router();
+
+router.get('/api/trips', (_req, res, next) => {
+  knex('trips')
+    .orderBy('name')
+    .then((rows) => {
+      const trips = camelizeKeys(rows);
+
+      res.send(trips);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const boom = require('boom');
 const express = require('express');
 const knex = require('../knex');
-const { camelizeKeys } = require('humps');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 const router = express.Router();
+
+router.get('/api/users', (_req, res, next) => {
+  knex('users')
+    .orderBy('name')
+    .then((rows) => {
+      const users = camelizeKeys(rows);
+
+      res.send(users);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
